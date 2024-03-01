e7baa54801eed227793f9a9a1535a402a38f79d4
prikol769 <prikol769@gmail.com>
2024-03-01 10:59:49 -0600

    added register user action

--- actions/register.ts
+++ actions/register.ts
@@ -1,15 +1,37 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
 
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validetedFields = RegisterSchema.safeParse(values);
 
-  if (!validetedFields) {
+  if (!validetedFields.success) {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validetedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  //ToDO  send verf token email
+
+  return { success: "User created!" };
 };
