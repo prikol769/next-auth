32739b4dd44257330a2458503ba3ae0b7642b6ac
prikol769 <prikol769@gmail.com>
2024-03-03 20:49:01 -0600

    added linkAccount event

--- auth.ts
+++ auth.ts
@@ -24,8 +24,16 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      });
+    },
+  },
   callbacks: {
-    session({ token, session }) {
+    async session({ token, session }) {
       if (token.sub && session.user) {
         session.user.id = token.sub;
       }
