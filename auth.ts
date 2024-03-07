9c20dd93699ae83aaed1d2d106b41343ebbb029d
prikol769 <prikol769@gmail.com>
2024-03-07 16:44:22 -0600

    added to session isOAuth,

--- auth.ts
+++ auth.ts
@@ -6,6 +6,7 @@ import { getUserById } from "@/data/user";
 import { db } from "./lib/db";
 import authConfig from "@/auth.config";
 import { getTwoFactorConfirmationByUserId } from "./data/two-factor-confirmation";
+import { getAccountByUserId } from "./data/account";
 
 declare module "next-auth" {
   interface User {
@@ -18,6 +19,9 @@ declare module "next-auth/jwt" {
   interface JWT {
     role: "ADMIN" | "USER";
     isTwoFactorEnabled: boolean;
+    sub: string;
+    email: string | null;
+    isOAuth: boolean;
   }
 }
 
@@ -67,16 +71,16 @@ export const {
       return true;
     },
     async session({ token, session }) {
-      if (token.sub && session.user) {
-        session.user.id = token.sub;
-      }
-
-      if (token.role && session.user) {
-        session.user.role = token.role;
-      }
-
       if (session.user) {
+        if (token.email) {
+          session.user.email = token.email;
+        }
+
         session.user.isTwoFactorEnabled = token.isTwoFactorEnabled;
+        session.user.name = token.name;
+        session.user.role = token.role;
+        session.user.id = token.sub;
+        session.user.isOAuth = token.isOAuth;
       }
 
       return session;
@@ -88,6 +92,11 @@ export const {
 
       if (!existingUser) return token;
 
+      const existingAccount = await getAccountByUserId(existingUser.id);
+
+      token.isOAuth = !!existingAccount;
+      token.name = existingUser.name;
+      token.email = existingUser.email;
       token.role = existingUser.role;
       token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
