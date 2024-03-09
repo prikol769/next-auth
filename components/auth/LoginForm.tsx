fc9500c94523b84fdfdb2c01c1daa39e2a9cccce
prikol769 <prikol769@gmail.com>
2024-03-08 20:26:50 -0600

    added callbackUrl to login

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -25,6 +25,8 @@ import Link from "next/link";
 
 const LoginForm = () => {
   const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl");
+
   const urlError =
     searchParams.get("error") === "OAuthAccountNotLinked"
       ? "Email already in use with different provider!"
@@ -48,7 +50,7 @@ const LoginForm = () => {
     setSuccess("");
 
     startTransition(() => {
-      login(values)
+      login(values, callbackUrl)
         .then((data) => {
           if (data?.error) {
             form.reset();
